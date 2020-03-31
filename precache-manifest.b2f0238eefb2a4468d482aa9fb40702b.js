6ce420f416d98d6861c82d008aa22d47c19e550b
Kuballoo <kuballo.g@gmail.com>
2020-03-31 20:14:29 +0200

    Updates

---
+++ precache-manifest.b2f0238eefb2a4468d482aa9fb40702b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4d14cc759e3b3dab0b13426497f6f9",
+    "url": "/React1/index.html"
+  },
+  {
+    "revision": "582cc95ff360e3ae85ba",
+    "url": "/React1/static/js/2.5d5873fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React1/static/js/2.5d5873fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44562a4de127f4a49284",
+    "url": "/React1/static/js/main.ff55e2b6.chunk.js"
+  },
+  {
+    "revision": "c9694dfdb345c4a3eff3",
+    "url": "/React1/static/js/runtime-main.e8f3a504.js"
+  }
+]);
